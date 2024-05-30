b5903613ea96c40970ac6faf687dc097e2fe6277
samatov-rsk <samativrsk@gmail.com>
2024-05-30 18:27:09 +0300

    Init commit

---
+++ src/main/java/com/samatov/inventoryservicebar/InventoryServiceBarApplication.java
@@ -0,0 +1,13 @@
+package com.samatov.inventoryservicebar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceBarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceBarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samatov/inventoryservicebar/InventoryServiceBarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samatov.inventoryservicebar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceBarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
