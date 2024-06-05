0b05b80e0b53a5f24a0a06b2120bbb0707386a2b
samatov-rsk <116744883+samatov-rsk@users.noreply.github.com>
2024-06-05 15:39:24 +0300

    Исправить все замечания которые я накидал
    После всех исправлений удалить все мои комментарии

--- src/main/java/com/samatov/inventoryservicebar/service/BeerService.java
+++ src/main/java/com/samatov/inventoryservicebar/service/BeerService.java
@@ -25,32 +25,37 @@ public class BeerService {
 
     @Autowired
     private BeerRepository beerRepository;
-
+    
+    //TODO Пиши правильно комментарии (Метод для получениЯ всего пивА) - исправить 
     // Метод для получение все пиво
     public List<Beer> getAllBeers() {
         return beerRepository.findAll();
     }
-
-    // Метод для сохрание пиво
+    
+    //TODO Пиши правильно комментарии (Метод для сохранениЯ пивА) - исправить 
+    // Метод для сохрание пиво 
     public void saveBeer(Beer beer) {
         beerRepository.save(beer);
     }
 
-    // Метод для удаление все пиво по айдишнику
+    //TODO Пиши правильно комментарии (Метод для удалениЯ пивА по айдишнику) - исправить 
+    // Метод для удаление пива по айдишнику
     public void deleteBeerById(String id) {
         beerRepository.deleteById(id);
     }
 
+    //TODO Пиши правильно комментарии (Метод для удалениЯ всего пива. Пиши или пиво или сущность) - исправить 
     // Метод для удаление всех сущностей
     public void deleteAllBeers() {
         beerRepository.deleteAll();
     }
-
+    
+    //TODO Пиши правильно комментарии (Метод для изменениЯ пивА по айдишнику) - исправить метод нет айдишника по которому мы меняем
     // Метод для измененение пиво
     public void updateBeer(Beer beer) {
         beerRepository.save(beer);
     }
-
+    //TODO Пиши правильно комментарии (Метод для нахождениЯ пивА по айдишнику) - исправить метод, метод должен возвращать сущность с БД
     // Метод для нахождение пиво по айдишнику
     public void findBeerById(String id) {
         beerRepository.findById(id);
